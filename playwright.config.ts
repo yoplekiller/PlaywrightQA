31dfbd950278f615faaa214262fc635155ecad6f
임재민 <jmlim9244@gmail.com>
2025-05-18 21:09:34 +0900

    fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,6 +23,7 @@ export default defineConfig({
         headless: true,
         viewport: { width: 1280, height: 720 },
         ignoreHTTPSErrors: true,
+        trace: 'on',
         screenshot: 'only-on-failure',
         video: 'retain-on-failure',
         baseURL: 'https://www.kurly.com/main',
