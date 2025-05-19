1a4d06bfaa7d87564ba6483bc450610523b0b0f9
임재민 <jmlim9244@gmail.com>
2025-05-19 19:33:38 +0900

    fix

--- tests/ui/ui_cart_product_cancel.spec.ts
+++ tests/ui/ui_cart_product_cancel.spec.ts
@@ -1,45 +1,45 @@
-// import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
-// test('test', async ({ page }) => {
+test('장바구니에 물건 담고 선택 취소까지', async ({ page }) => {
 
-//   await page.goto('https://www.kurly.com/main');
-//   // 더 견고하게: 버튼 텍스트로 찾기 (뷰티컬리)
-//   const myButton = page.locator('button:has-text("뷰티컬리")');
-//   await expect(myButton).toBeVisible({ timeout: 7000 });
-//   await myButton.click();
+  await page.goto('https://www.kurly.com/main');
+  // 더 견고하게: 버튼 텍스트로 찾기 (뷰티컬리)
+  const myButton = page.locator('button:has-text("뷰티컬리")');
+  await expect(myButton).toBeVisible({ timeout: 7000 });
+  await myButton.click();
 
-//   await page.getByRole('textbox', { name: '뷰티 상품을 검색하세요' }).fill('과자');
-//   await page.getByRole('textbox', { name: '뷰티 상품을 검색하세요' }).press('Enter');
+  await page.getByRole('textbox', { name: '뷰티 상품을 검색하세요' }).fill('과자');
+  await page.getByRole('textbox', { name: '뷰티 상품을 검색하세요' }).press('Enter');
   
-//   // XPath로 '담기' 버튼을 첫 번째로 찾기
-//   const targetButton = page.locator("xpath=(//button[@type='button'][contains(text(),'담기')])[1]");
-//   await expect(targetButton).toBeVisible({ timeout: 7000 });
-//   await targetButton.click();
+  // XPath로 '담기' 버튼을 첫 번째로 찾기
+  const targetButton = page.locator("xpath=(//button[@type='button'][contains(text(),'담기')])[1]");
+  await expect(targetButton).toBeVisible({ timeout: 7000 });
+  await targetButton.click();
 
   
-//   // '수량올리기' 버튼이 없을 경우 예외 방지 (count로 체크)
-//   const nextBtn = page.locator("xpath=(//button[@aria-label='수량올리기'])[1]");
-//   if (await nextBtn.count() > 0) {
-//     await nextBtn.click();
-//   } else {
-//     console.warn("수량올리기 버튼이 존재하지 않습니다.");
-//   }
+  // '수량올리기' 버튼이 없을 경우 예외 방지 (count로 체크)
+  const nextBtn = page.locator("xpath=(//button[@aria-label='수량올리기'])[1]");
+  if (await nextBtn.count() > 0) {
+    await nextBtn.click();
+  } else {
+    console.warn("수량올리기 버튼이 존재하지 않습니다.");
+  }
 
-//   const cartAddBtn = page.locator("xpath=(//button[@class='css-ahkst0 e4nu7ef3'])[1]");
-//   await expect(cartAddBtn).toBeVisible({ timeout: 7000 });
-//   await cartAddBtn.click();
+  const cartAddBtn = page.locator("xpath=(//button[@class='css-ahkst0 e4nu7ef3'])[1]");
+  await expect(cartAddBtn).toBeVisible({ timeout: 7000 });
+  await cartAddBtn.click();
 
 
-//   const cartBtn = page.locator("xpath=(//button[@class='css-g25h97 e14oy6dx1'])")
-//   await expect(cartBtn).toBeVisible({ timeout: 7000 });
-//   await cartBtn.click();
+  const cartBtn = page.locator("xpath=(//button[@class='css-g25h97 e14oy6dx1'])")
+  await expect(cartBtn).toBeVisible({ timeout: 7000 });
+  await cartBtn.click();
 
-//   await expect(page).toHaveURL('https://www.kurly.com/cart');
+  await expect(page).toHaveURL('https://www.kurly.com/cart');
 
-//   // await page.getByRole('button', { name: '2', exact: true }).click();
-//   await page.getByText('전체선택 1/').click();
-//   await page.getByText('전체선택 0/').click();
-//   await page.getByRole('button', { name: '선택삭제' }).click();
-//   await page.getByRole('button', { name: 'confirm-button' }).click();
-//   await expect(page.getByText("전체선택 0/0선택삭제장바구니에 담긴 상품이 없습니다")).toBeVisible();
-// });
+  // await page.getByRole('button', { name: '2', exact: true }).click();
+  await page.getByText('전체선택 1/').click();
+  await page.getByText('전체선택 0/').click();
+  await page.getByRole('button', { name: '선택삭제' }).click();
+  await page.getByRole('button', { name: 'confirm-button' }).click();
+  await expect(page.getByText("전체선택 0/0선택삭제장바구니에 담긴 상품이 없습니다")).toBeVisible();
+});
