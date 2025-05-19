3df7bb0e57fe610def52d93e9ec90533f4a50526
임재민 <jmlim9244@gmail.com>
2025-05-20 07:39:07 +0900

    fix

--- tests/ui/ui_10th_popup.spec.ts
+++ tests/ui/ui_10th_popup.spec.ts
@@ -3,10 +3,9 @@ import { test, expect } from '@playwright/test';
 test('10주년 팝업 기능', async ({ page }) => {
   await page.goto('https://www.kurly.com/main');
   await page.getByRole('paragraph').filter({ hasText: /^$/ }).getByRole('link').click();
-  await page.waitForLoadState('networkidle');
-  await expect(page.getByText('10주년 굿즈')).toBeVisible();
-  
-  
+  await page.waitForTimeout(2000); // Wait for 2 seconds to observe the change
+  await expect(page).toHaveURL('https://event.kurly.com/lego/event/2023/0911/join/coupon');
+
   });
   
 
