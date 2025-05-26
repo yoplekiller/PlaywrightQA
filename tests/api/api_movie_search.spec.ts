c7ba9a0b49949d147c71cc2cc25a21b1ee47bed0
임재민 <jmlim9244@gmail.com>
2025-05-26 21:50:48 +0900

    fix

--- tests/api/api_movie_search.spec.ts
+++ tests/api/api_movie_search.spec.ts
@@ -1,34 +1,30 @@
-// import { test, expect, request } from '@playwright/test';
-// import { allure } from 'allure-playwright';
-// import dotenv from 'dotenv';
+import { test, expect, request } from '@playwright/test';
+import { allure } from 'allure-playwright';
+import dotenv from 'dotenv';
 
-// dotenv.config();
+dotenv.config();
 
-<<<<<<< HEAD
-// const BASE_URL = process.env.TMDB_BASE_URL || 'https://api.themoviedb.org/';
-// const API_KEY = process.env.TMDB_API_KEY!;
-=======
 const BASE_URL = process.env.TMDB_BASE_URL || 'https://api.themoviedb.org/3';
 const API_KEY = process.env.TMDB_API_KEY!;
->>>>>>> develop
 
-// test('🎬 인기 영화 목록 조회 → 응답 200 및 결과 리스트 확인', async () => {
-//   const apiContext = await request.newContext({ baseURL: BASE_URL });
 
-//   const endpoint = `3/movie/popular?api_key=${API_KEY}`;
-//   const response = await apiContext.get(endpoint);
-//   const status = response.status();
-//   const data = await response.json();
+test('🎬 인기 영화 목록 조회 → 응답 200 및 결과 리스트 확인', async () => {
+  const apiContext = await request.newContext({ baseURL: BASE_URL });
 
-//   // ✅ 테스트 검증
-//   expect(status).toBe(200);
-//   expect(data).toHaveProperty('results');
-//   expect(Array.isArray(data.results)).toBe(true);
-//   expect(data.results.length).toBeGreaterThan(0);
+  const endpoint = `3/movie/popular?api_key=${API_KEY}`;
+  const response = await apiContext.get(endpoint);
+  const status = response.status();
+  const data = await response.json();
 
-//   // ✅ Allure 첨부
-//   allure.attachment('인기 영화 목록 응답', JSON.stringify(data, null, 2), 'application/json');
+  // ✅ 테스트 검증
+  expect(status).toBe(200);
+  expect(data).toHaveProperty('results');
+  expect(Array.isArray(data.results)).toBe(true);
+  expect(data.results.length).toBeGreaterThan(0);
 
-//   // ✅ 결과 콘솔 출력
-//   console.log(`🎉 인기 영화 ${data.results.length}건 조회됨`);
-// });
+  // ✅ Allure 첨부
+  allure.attachment('인기 영화 목록 응답', JSON.stringify(data, null, 2), 'application/json');
+
+  // ✅ 결과 콘솔 출력
+  console.log(`🎉 인기 영화 ${data.results.length}건 조회됨`);
+});
