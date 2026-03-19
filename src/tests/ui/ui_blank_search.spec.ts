ceaad4b63d5760bd20c9f527ae8e833f7d3f90b0
LIMJAEMIN <159503436+yoplekiller@users.noreply.github.com>
2026-03-19 16:15:33 +0900

    delete api-yaml

--- src/tests/ui/ui_blank_search.spec.ts
+++ src/tests/ui/ui_blank_search.spec.ts
@@ -1,17 +1,17 @@
 import { test, expect } from '@playwright/test';
 import { MainPage } from '../../pages/MainPage';
 
-test('공백 입력 시 팝업 노출 확인', async ({ page }, testInfo) => {
-    const mainPage = new MainPage(page);
+test('🔲 공백 입력 시, 팝업 노출 확인', async ({ page }, testInfo) => {
+    const mainpage = new MainPage(page);
 
     // 메인 페이지 접속
-    await mainPage.goto('/main');
-    await mainPage.setViewportSize(1280, 720);
+    await page.goto('/main');
+    await mainpage.setViewportSize(1280, 720);
 
     // 검색창에 공백 입력 후 검색
-    await mainPage.searchGoods(" ");
+    await mainpage.searchGoods(" ");
 
     // 팝업 노출 확인
-    const isPopupVisible = await mainPage.isBlankSearchPopupVisible();
+    const isPopupVisible = await mainpage.isBlankSearchPopupVisible();
     expect(isPopupVisible).toBeTruthy();
 });
